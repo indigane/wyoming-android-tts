271d90f84ee5c7caf9b6d259e279373bb1d6b5a5
indigane <indigane@users.noreply.github.com>
2025-06-28 12:22:06 +0300

    Bump target SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "home.wyoming_android_tts"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "home.wyoming_android_tts"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
